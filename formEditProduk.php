d63aec625a8b38559166345d075c66fe0617e59c
Abiwardani <55235155+abiwardanii@users.noreply.github.com>
2020-09-17 00:07:47 +0700

    Update formEditProduk.php

--- formEditProduk.php
+++ formEditProduk.php
@@ -17,20 +17,30 @@ $id = $_GET['id'];
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
-    <form action="prosesEditProduk.php?id=<?php echo $id; ?>" method="post">
-        <label>Nama Produk</label>
-        <div><input type="text" name="nama_produk" value="<?php echo $row['nama_produk'] ?>"> </div>
+    <div class="container">
+        <form action="prosesEditProduk.php?id=<?php echo $id; ?>" method="post">
+        <div class="form-group">
+            <label>Nama Produk</label>
+            <input type="text" name="nama_produk" value="<?php echo $row['nama_produk'] ?>" class="form-control">
+        </div>
 
-        <label>Ketarangan</label>
-        <div><input type="text" name="keterangan" value="<?php echo $row['keterangan'] ?>"></div>
+        <div class="form-group">
+            <label>Ketarangan</label>
+            <input type="text" name="keterangan" value="<?php echo $row['keterangan'] ?>" class="form-control">
+        </div>
 
-        <label>Harga</label>
-        <div><input type="text" name="harga" value="<?php echo $row['harga'] ?>"></div>
+        <div class="form-group">
+            <label>Harga</label>
+            <input type="text" name="harga" value="<?php echo $row['harga'] ?>" class="form-control">
+        </div>
 
-        <label>Jumlah</label>
-        <div><input type="text" name="jumlah" value="<?php echo $row['jumlah'] ?>"></div> 
-        
-        <div><input type="submit" name="beli" value="Buy"></div>
-    </form>
+        <div class="form-group">
+            <label>Jumlah</label>
+            <input type="text" name="jumlah" value="<?php echo $row['jumlah'] ?>" class="form-control">
+        </div>
+            
+            <button input type="submit" name="edit" value="edit" class="btn btn-primary">Edit</button>
+        </form>
+    </div>
 </body>
 </html>
